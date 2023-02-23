1dd79c05bc907eb3d52104d6a0260e6d2b585252
JHidalgo94 <124218640+JHidalgo94@users.noreply.github.com>
2023-02-23 10:28:23 -0600

    gg

--- Proyecto/Entidades/Class1.cs
+++ Proyecto/Entidades/Class1.cs
@@ -1,13 +1,25 @@
 ﻿namespace Entidades
 {
-    public class Class1
+    public class login
     {
         public string contraseña { get; set; }
 
         public string rol { get; set; }
 
-        public string usuario { get; set; }
+        public string codigousuario { get; set; }
+
+        public DateTime Myproperty { get; set; }
 
 
+        public login()
+        {
+        }
+
+        public login(string contraseña, string rol, string usuario)
+        {
+            this.contraseña = contraseña;
+            this.rol = rol;
+            this.codigousuario = usuario;
+        }
     }
 }
---
+++ Proyecto/Entidades/Class2.cs
@@ -0,0 +1,21 @@
+﻿namespace Entidades
+{
+    public class usuario
+    {
+        public string codigousuario { get; set; }
+
+        public string nombre { get; set; }
+
+        public string contraseña { get; set; }
+
+        public string correo { get; set; }
+
+        public string rol { get; set; }
+
+        public DateTime fechadecreacion { get; set; }
+
+    }
+
+
+
+}
