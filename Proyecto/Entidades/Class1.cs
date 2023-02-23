2a094185599f87facb534ea7cab91d4e5ca62a41
JHidalgo94 <124218640+JHidalgo94@users.noreply.github.com>
2023-02-23 08:20:57 -0600

    ff

---
+++ Proyecto/Entidades/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Entidades
+{
+    public class Class1
+    {
+        public string contraseña { get; set; }
+
+        public string rol { get; set; }
+
+        public string usuario { get; set; }
+
+
+    }
+}
